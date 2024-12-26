f9146cb163e4a6d5f6fda2080003855676b0a50b
Santhosh S <santhosh.sivanath@outlook.com>
2024-12-26 18:57:28 +0530

    Move away from deprecated way of creating Inline policies (#5)

--- modules/role/main.tf
+++ modules/role/main.tf
@@ -2,23 +2,22 @@ resource "aws_iam_role" "this" {
   name                 = var.name
   path                 = var.path
   assume_role_policy   = data.aws_iam_policy_document.assume_role_policy.json
-  managed_policy_arns  = [for policy in data.aws_iam_policy.managed_policies : policy.arn]
   permissions_boundary = one(data.aws_iam_policy.permissions_boundary[*].arn)
 
-  dynamic "inline_policy" {
-    for_each = length(local.inline_policies) == 0 ? [null] : []
-
-    content {}
-  }
-
-  dynamic "inline_policy" {
-    for_each = keys(local.inline_policies)
-
-    content {
-      name   = inline_policy.value
-      policy = data.aws_iam_policy_document.inline_policy[inline_policy.value].json
-    }
-  }
-
   tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  for_each = data.aws_iam_policy.managed_policies
+
+  role       = aws_iam_role.this.id
+  policy_arn = each.value.arn
+}
+
+resource "aws_iam_role_policy" "this" {
+  for_each = data.aws_iam_policy_document.inline_policy
+
+  name   = each.key
+  role   = aws_iam_role.this.id
+  policy = each.value.json
 }
